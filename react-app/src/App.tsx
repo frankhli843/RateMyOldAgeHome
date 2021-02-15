f98fa55eaaa709c6295077c421af6aeaac6144f0
frankhli843 <lifrank1994@gmail.com>
2021-02-15 18:29:14 -0500

    adding react-router

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
 import './Styles/base.scss';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-
-			</header>
-		</div>
+		<Router>
+			<div>
+				<NavigationLinks/>
+				<NavigationSwitch />
+			</div>
+		</Router>
 	);
+}
+
+function NavigationLinks(){
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+				<li>
+					<Link to="/users">Users</Link>
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+function NavigationSwitch(){
+	return (
+		<Switch>
+			<Route path="/about">
+				<About />
+			</Route>
+			<Route path="/users">
+				<Users />
+			</Route>
+			<Route path="/">
+				<Home />
+			</Route>
+		</Switch>
+	)
+}
+
+function Home() {
+	return <h2>Home</h2>;
+}
+
+function About() {
+	return <h2>About</h2>;
+}
+
+function Users() {
+	return <h2>Users</h2>;
 }
 
 export default App;
