26603430da4366c9a2ba48868d97ba38b13b2945
Henrik Hey <henrikheywork@gmail.com>
2021-02-15 22:45:51 -0500

    more code

--- go-backend/main.go
+++ go-backend/main.go
@@ -6,15 +6,30 @@ import (
 	"app/services"
 )
 
+const PORT string = ":4444"
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("react-build")))
 
-	// Routes
+	// USer
 	http.HandleFunc("/register", services.Register)
 	http.HandleFunc("/login", services.Login)
 	http.HandleFunc("/logout", services.Logout)
 	http.HandleFunc("/deleteAccount", services.DeleteAccount)
 
-	err := http.ListenAndServe(":4444", nil)
+	// Home
+	http.HandleFunc("/createHome", services.CreateHome)
+	http.HandleFunc("/getHomeList", services.GetHomeList)
+	http.HandleFunc("/getHomeByID", services.GetHomeByID)
+	http.HandleFunc("/updateHome", services.UpdateHome)
+	http.HandleFunc("/deleteHome", services.DeleteHome)
+
+	// Review
+	http.HandleFunc("/addReview", services.AddReview)
+	http.HandleFunc("/updateReview", services.UpdateReview)
+	http.HandleFunc("/removeReview", services.RemoveReview)
+
+
+	err := http.ListenAndServe(PORT, nil)
 	if err != nil { fmt.Println(err) }
 }
--- go-backend/services/homeManager.go
+++ go-backend/services/homeManager.go
@@ -7,35 +7,35 @@ import (
 
 // Home Routes
 func CreateHome(w http.ResponseWriter, r *http.Request) {
-	if util.IsRequestValid(w, r, "/register") {
+	if util.IsRequestValid(w, r, "/createHome") {
 		// Logic
 	}
 	return;
 }
 
 func GetHomeList(w http.ResponseWriter, r *http.Request) {
-	if util.IsRequestValid(w, r, "/register") {
+	if util.IsRequestValid(w, r, "/getHomeList") {
 		// Logic
 	}
 	return;
 }
 
 func GetHomeByID(w http.ResponseWriter, r *http.Request) {
-	if util.IsRequestValid(w, r, "/register") {
+	if util.IsRequestValid(w, r, "/getHomeByID") {
 		// Logic
 	}
 	return;
 }
 
 func UpdateHome(w http.ResponseWriter, r *http.Request) {
-	if util.IsRequestValid(w, r, "/register") {
+	if util.IsRequestValid(w, r, "/updateHome") {
 		// Logic
 	}
 	return;
 }
 
 func DeleteHome(w http.ResponseWriter, r *http.Request) {
-	if util.IsRequestValid(w, r, "/register") {
+	if util.IsRequestValid(w, r, "/deleteHome") {
 		// Logic
 	}
 	return;
